0f859fe00f2511993892b41a666c2a742dccfcc7
Om Handge <omhandge17@gmail.com>
2026-07-24 14:20:22 +0530

    Fix requirements.txt

--- static/train_model.py
+++
@@ -1,9 +0,0 @@
-# Import required libraries
-import pandas as pd
-import numpy as np
-# Load datasets
-fake_news = pd.read_csv("dataset/Fake.csv")
-true_news = pd.read_csv("dataset/True.csv")
-
-print(fake_news.head())
-print(true_news.head())
